a287707fd268d3be6843c24034009017b2998ea6
kxusx <71188684+kxusx@users.noreply.github.com>
2022-07-07 08:22:10 +0400

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import {DOlaProvider} from '../context/DOlaContext' 
-function MyApp({ Component, pageProps }) {
-  return <DOlaProvider><Component {...pageProps} /></DOlaProvider>
-}
-
-export default MyApp
